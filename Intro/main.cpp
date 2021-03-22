568f84b29db5a82263c97237a4ce395cfb1aeff7
Anna <goryachevaanny@gmail.com>
2021-03-22 19:22:01 +0300

    Initial Commit

---
+++ Intro/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Intro
+//
+//  Created by Анна Горячева on 22.03.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
